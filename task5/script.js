aaa700c38b1299a6d6329ced90a390f9b6721993
Sumeya472 <sumeyaseid703@gmail.com>
2025-09-13 21:18:44 +0300

    Create script.js

---
+++ task5/script.js
@@ -0,0 +1,4 @@
+alert("Welcome to this page!");
+console.log("Hello! The page is fully loaded. Enjoy your visit!");
+let name = prompt("Please enter your name:");
+alert("Hi " + name + "! We're really glad to have you here ");
